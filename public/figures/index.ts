1dbdbb9d1ff12224ea50237886192233ef443daf
Miłosz Buchacz <miloszbuchacz@gmail.com>
2026-01-06 14:12:01 +0100

    created a landing page window, added few photos

---
+++ public/figures/index.ts
@@ -0,0 +1,33 @@
+import bulb1 from "./bulb1.png";
+import bulb2 from "./bulb2.png";
+import bulb3 from "./bulb3.png";
+import bulb4 from "./bulb4.png";
+import bulb5 from "./bulb5.png";
+
+import gible1 from "./gible1.png";
+import gible2 from "./gible2.png";
+import gible3 from "./gible3.png";
+import gible4 from "./gible4.png";
+import gible5 from "./gible5.png";
+import gible6 from "./gible6.png";
+
+import gligar1 from "./gligar1.png";
+import gligar2 from "./gligar2.png";
+import gligar3 from "./gligar3.png";
+
+export const figures = [
+  bulb1,
+  bulb2,
+  bulb3,
+  bulb4,
+  bulb5,
+  gible1,
+  gible2,
+  gible3,
+  gible4,
+  gible5,
+  gible6,
+  gligar1,
+  gligar2,
+  gligar3,
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,33 @@
 import Background from "./components/Background/Background";
+import Button from "./components/Button/Button";
+import Drower from "./components/Drower/Drower";
 import TopBar from "./components/topBar/TopBar";
 import HomePage from "./pages/homePage/Page";
+
+import "./components/topBar/TopBar.css";
 
 export default function Home() {
   return (
     <>
+      <Background />
       <TopBar />
-      <HomePage />
-      {/* <div style={{ zIndex: "1" }}>
-        <Background />
-      </div> */}
+      <main style={{ position: "relative", zIndex: 2 }}>
+        <HomePage />
+      </main>
+      <Drower>
+        <Button className="custom-button-styles" url="/home">
+          home
+        </Button>
+        <Button className="custom-button-styles" url="/contact">
+          contact
+        </Button>
+        <Button className="custom-button-styles" url="/aboutUs">
+          about us
+        </Button>
+        <Button className="custom-button-styles" url="/projects">
+          our work
+        </Button>
+      </Drower>
     </>
   );
 }
